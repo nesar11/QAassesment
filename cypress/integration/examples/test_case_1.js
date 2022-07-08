f1bcf6109e5de00e9f625e3d664704b97f620ce3
nesar11 <nesar.uddin100@gmail.com>
2022-07-08 15:15:30 +0800

    set up cypress

---
+++ cypress/integration/examples/test_case_1.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+context('Window', () => {
+  beforeEach(() => {
+    cy.visit('https://www.carsome.my/')
+  })
+  it('cy.title() - get the title', () => {
+    
+
+
+  })
+
+})
