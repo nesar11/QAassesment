1766b8f6a99b5787f9c7596fa0ccdd0552a03fd3
nesar11 <nesar.uddin100@gmail.com>
2022-07-08 20:34:18 +0800

    get latest navigated url

--- cypress/integration/examples/test_case_1.js
+++ cypress/integration/examples/test_case_1.js
@@ -8,10 +8,15 @@ context('Window', () => {
     
     // visit the website and click on buy car class
     cy.get('.buy-car').click();
-    
+
     // curent url will be https://www.carsome.my/buy-car  and from the menu box brand select as Perodua then click
     cy.get('.menu-box-brand').contains("Perodua").click();
+    
+    // current navigated url https://www.carsome.my/buy-car/perodua
+    cy.url().should('include', 'buy-car/perodua');
 
+
+    
   })
 
 })
