8c2a8730bdd5a33a03c9ab3a1d13745515b39a0d
nesar11 <nesar.uddin100@gmail.com>
2022-07-09 16:51:42 +0800

    select the interest as local

--- cypress/integration/examples/test_case_2 car recommanded.js
+++ cypress/integration/examples/test_case_2 car recommanded.js
@@ -7,10 +7,15 @@ context('Window', () => {
     it('cy.title() - get the title', () => {
       // visit the website and click on buy car class
       cy.get('.buy-car').click();
+
       // From the top right navigation Buy car dropdown most bottom a special cta button name recommand for me a car on click create parent tab
       // invoke inside insert removeAttr and second parameter as target then on click in the child tab will open the recomand page
       cy.get('.car-finder-menu-wrapper').invoke('removeAttr', 'target').click();
+
       // select the buget from array under Rm 30K
+      cy.get('.content-list-wrapper').children('.content-item').eq('0').click();
+
+      // select the from array first item as 0 which local
       cy.get('.content-list-wrapper').children('.content-item').eq('0').click();
       
     })
